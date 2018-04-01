d3aa78c7c1c8ba5859bb4fa2181e108fe4f73865
pxl <pxl@Lenovo-pc>
2018-04-01 18:13:44 +0800

    test update

--- dubbo-provider/src/main/java/org/dubbo/provider/App.java
+++ dubbo-provider/src/main/java/org/dubbo/provider/App.java
@@ -1,11 +1,18 @@
 package org.dubbo.provider;
 
+import java.io.IOException;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main(String[] args) throws IOException {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"dubbo-provider.xml"});
+		context.start();
+		System.out.println("provider server started.");
+		System.in.read();
 	}
 }
